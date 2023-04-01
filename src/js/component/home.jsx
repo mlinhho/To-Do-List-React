9f7b2a1f085c861b7d45019dc862204b74406702
mlinhho <mailinhtranho8@gmail.com>
2023-04-01 01:33:49 +0000

    complete delete task

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,50 +1,46 @@
 import React, { useState } from "react";
 
-//create your first component
+// include images into your bundle
+import rigoImage from "../../img/rigo-baby.jpg";
+
+// create your first component
 const Home = () => {
-  function _handleKeyDown(e) {
-    if (e.key === "Enter") {
-      setTodos(toDos.concat([inputValue]));
-      setInputValue("");
-    }
-  }
   const [inputValue, setInputValue] = useState("");
-  const [toDos, setTodos] = useState([]);
-  const [isShown, setIsShown] = useState(false);
+  const [todos, setTodos] = useState([]);
+
   return (
-    <div className="text-center">
-      <h1 className="text-center">TODOS</h1>
-      <div className="notePad">
-        <ul className="w-100">
+    <div className="container">
+      <h1>To Do List</h1>
+      <ul>
+        <li>
+          <input
+            type="text"
+            onChange={(e) => setInputValue(e.target.value)}
+            value={inputValue}
+            onKeyUp={(e) => {
+              if (e.key === "Enter") {
+                setTodos(todos.concat(inputValue));
+                setInputValue("");
+              }
+            }}
+            placeholder="Add items here..."
+          ></input>
+        </li>
+        {todos.map((item, index) => (
           <li>
-            <input
-              type="text"
-              id="addToDo"
-              placeholder="Add task"
-              onKeyDown={_handleKeyDown}
-              onChange={(e) => setInputValue(e.target.value)}
-              value={inputValue}
-            />
+            {item}{" "}
+            <i
+              className="fas fa-trash-alt"
+              onClick={() =>
+                setTodos(
+                  todos.filter((t, currentIndex) => index != currentIndex)
+                )
+              }
+            ></i>
           </li>
-
-          {toDos.map((item, index) => (
-            <li
-              onMouseEnter={() => setIsShown(true)}
-              onMouseLeave={() => setIsShown(false)}
-              className="w-100 d-flex"
-            >
-              <p className="w-auto">{item}</p>
-              {isShown === true ? (
-                <i className="fa-solid fa-xmark ms-auto"></i>
-                ) : (
-                ""
-              )}
-            </li>
-          ))}
-        </ul>
-
-        <div> {toDos.length} item left</div>
-      </div>
+        ))}
+      </ul>
+      <div>{todos.length} Tasks</div>
     </div>
   );
 };
